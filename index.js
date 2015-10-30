809f4f9387682350d09176ed479ca4ec64773fbd
unknown <G04501867@BDSPIND70196324.client.barclayscorp.com>
2015-10-30 12:25:38 +0530

    '29-10-2015'

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('Hi');
+var http = require('http');
+
+var server = http.createServer(function(req, res) {
+    res.writeHead(200);
+    res.end('Hello Http');
+});
+server.listen(process.env.OPENSHIFT_NODEJS_PORT || 3000);
